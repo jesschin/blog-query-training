43471619df3859892c73171c30b5f2b98ef432ee
Paula Bianca Torre <paula.torre@teamcodeflux.com>
2014-06-29 15:33:02 +0800

    Allow user to add comment on show post view.
    Signed-off-by: Paula Bianca Torre <paula.torre@teamcodeflux.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts, :only => [:index, :show]
+  resources :posts, :only => [:index, :show] do
+    resources :comments, :only => [:new, :create]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
