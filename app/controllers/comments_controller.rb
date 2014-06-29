bb11aaff75753aca0b41959fe3d40a68a77397ad
Paula Bianca Torre <paula.torre@teamcodeflux.com>
2014-06-29 15:33:40 +0800

    Save comment.
    Signed-off-by: Paula Bianca Torre <paula.torre@teamcodeflux.com>

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,5 +3,12 @@ class CommentsController < ApplicationController
   end
 
   def create
+    post = Post.find(params[:post_id])
+    @comment = Comment.new(permitted_params.comment)
+    @comment.post = post
+
+    if @comment.save
+      redirect_to post_path(post)
+    end
   end
 end
